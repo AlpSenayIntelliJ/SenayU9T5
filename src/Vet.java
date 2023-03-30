3dbdb0cc41279e239a6e0e33e50d347febb1d85c
Alp Senay <alps2789@nycstudents.net>
2023-03-30 09:21:47 -0400

    working...

---
+++ src/Vet.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Vet {
+    private String name;
+    private ArrayList<Animal> clients;
+    public Vet(String name) {
+        this.name = name;
+        clients = new ArrayList<>();
+    }
+    public void addClient(Animal animal) {
+        if (clients.contains(animal)) {
+            System.out.println(animal.getName() + " is already a client of " + name);
+        } else {
+            System.out.println("Welcome to " + name + "'s office, " + animal.getName() + "!");
+            clients.add(animal);
+        }
+    }
+}
