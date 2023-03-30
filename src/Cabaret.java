cbfa0ae48e5d1dd4612f4f841eb3ea47f7d83ded
BT_3W2_21 <alps2789@nycstudents.net>
2023-03-30 11:41:01 -0400

    done!

--- src/Cabaret.java
+++ src/Cabaret.java
@@ -21,4 +21,56 @@ public class Cabaret {
             return true;
         }
     }
+
+    public boolean removePerformer(Performer performer) {
+        if (performers.contains(performer)) {
+            performers.remove(performer);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double averagePerformerAge() {
+        double sum = 0.0;
+        int count = 0;
+        for (Performer p : performers) {
+            sum += p.getAge();
+            count++;
+        }
+        return sum/count;
+    }
+
+    public ArrayList<Performer> performersOverAge(int minAge) {
+        ArrayList<Performer> arr = new ArrayList<>();
+        for (Performer p : performers) {
+            if (p.getAge() >= minAge) {
+                arr.add(p);
+            }
+        }
+        return arr;
+    }
+
+    public void groupRehearsal() {
+        for (Performer p : performers) {
+            System.out.println("REHEARSAL CALL! " + p.getName());
+            if (p instanceof Comedian) {
+                ((Comedian) p).rehearse(false);
+            } else {
+                p.rehearse();
+            }
+        }
+    }
+
+    public void cabaretShow() {
+        for (Performer p : performers) {
+            System.out.println("Welcome to the cabaret! Next act up... " + p.getName());
+            if (p instanceof Dancer) {
+                ((Dancer) p).pirouette(2);
+                p.perform();
+            } else {
+                p.perform();
+            }
+        }
+    }
 }
