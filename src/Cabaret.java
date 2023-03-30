576fdc988c2e4f2be27ebe646823c7298a2e96ce
Alp Senay <alps2789@nycstudents.net>
2023-03-30 11:03:45 -0400

    working...

---
+++ src/Cabaret.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Cabaret {
+    private String name;
+    private ArrayList<Performer> performers;
+    public Cabaret(String name) {
+        this.name = name;
+        performers = new ArrayList<>();
+    }
+    public String getName() {
+        return name;
+    }
+    public ArrayList<Performer> getPerformers() {
+        return performers;
+    }
+    public boolean addPerformer(Performer performer) {
+        if (performers.contains(performer)) {
+            return false;
+        } else {
+            performers.add(performer);
+            return true;
+        }
+    }
+}
